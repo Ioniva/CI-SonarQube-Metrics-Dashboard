200a0825760afb8d078be4b54492554f82be91ab
Ioniva <ioniva@icloud.com>
2024-09-13 14:27:49 +0300

    Fix Windows compatibility issue

--- prometheus/start_prometheus.sh
+++ prometheus/start_prometheus.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-# Define the URL for changing the SonarQube password
+
+# Define la URL para cambiar la contraseña de SonarQube
 SONARQUBE_URL="http://${SONARQUBE_USER}:${SONARQUBE_OLD_PASSWORD}@${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/users/change_password?login=${SONARQUBE_USER}&previousPassword=${SONARQUBE_OLD_PASSWORD}&password=${SONARQUBE_NEW_PASSWORD}"
 
-# Execute the wget command to change the SonarQube password
+# Espera 1 minuto antes de ejecutar el comando wget
+echo "Esperando 1 minuto antes de cambiar la contraseña de SonarQube..."
+sleep 60
+
+# Ejecuta el comando wget para cambiar la contraseña de SonarQube
 wget_output=$(wget --post-data='' "$SONARQUBE_URL" -O - 2>&1)
 wget_status=$?
 
-# Check if wget command was successful
+# Verifica si el comando wget fue exitoso
 if [[ $wget_status -ne 0 ]]; then
-  echo "Error: SonarQube password is already changed or there was a wget error."
-  echo "wget failed with status $wget_status. Output:"
+  echo "Error: La contraseña de SonarQube ya ha sido cambiada o hubo un error con wget."
+  echo "wget falló con el estado $wget_status. Salida:"
   echo "$wget_output"
 else
-  echo "SonarQube password changed successfully."
+  echo "La contraseña de SonarQube se cambió con éxito."
 fi
 
-# Start Prometheus service
-echo "Starting Prometheus..."
+# Inicia el servicio de Prometheus
+echo "Iniciando Prometheus..."
 exec prometheus --config.file=/etc/prometheus/prometheus.yml
--- sonarqube/check_sonarqube_health.sh
+++ sonarqube/check_sonarqube_health.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-# Define the URL
-URL="http://localhost:9000/api/system/status"
+# Define the URL using environment variables
+URL="http://${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/system/health"
 
-# Use curl to fetch the status and check if SonarQube is up
-if curl -s "$URL" | grep -q '"status":"UP"'; then
-    echo "SonarQube is up and running!"
-    exit 0
-else
-    echo "SonarQube is not up yet."
-    exit 1
-fi
+# Wait for SonarQube to be fully healthy
+echo "Checking if SonarQube is fully healthy at $URL..."
+
+# Loop until SonarQube returns "GREEN" for health status
+until curl -s "$URL" | grep -q '"status":"GREEN"'; do
+    echo "SonarQube is not fully healthy yet. Retrying in 10 seconds..."
+    sleep 10
+done
+
+# Once SonarQube is fully healthy
+echo "SonarQube is fully healthy and ready!"
+exit 0
