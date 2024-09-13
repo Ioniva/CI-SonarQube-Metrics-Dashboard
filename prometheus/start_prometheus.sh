8e680d4cdebc90a7686a861045cc6d5ec2f55e56
Ioniva <ioniva@icloud.com>
2024-09-13 11:12:34 +0300

    Normalize line endings

--- prometheus/start_prometheus.sh
+++ prometheus/start_prometheus.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# Define the URL for changing the SonarQube password
+SONARQUBE_URL="http://${SONARQUBE_USER}:${SONARQUBE_OLD_PASSWORD}@${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/users/change_password?login=${SONARQUBE_USER}&previousPassword=${SONARQUBE_OLD_PASSWORD}&password=${SONARQUBE_NEW_PASSWORD}"
 
-# Perform the wget operation
-wget_output=$(wget --post-data='' "http://${SONARQUBE_USER}:${SONARQUBE_OLD_PASSWORD}@${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/users/change_password?login=${SONARQUBE_USER}&previousPassword=${SONARQUBE_OLD_PASSWORD}&password=${SONARQUBE_NEW_PASSWORD}" -O - 2>&1)
+# Execute the wget command to change the SonarQube password
+wget_output=$(wget --post-data='' "$SONARQUBE_URL" -O - 2>&1)
 wget_status=$?
 
-# Check if wget was successful and log the output
-if [ $wget_status -ne 0 ]; then
-  echo "the sonarqube password is already changed, or wget error."
-  # echo "wget failed with status $wget_status. Output:"
-  # echo "$wget_output"
-  # exit 1
+# Check if wget command was successful
+if [[ $wget_status -ne 0 ]]; then
+  echo "Error: SonarQube password is already changed or there was a wget error."
+  echo "wget failed with status $wget_status. Output:"
+  echo "$wget_output"
 else
-  echo "wget succeeded."
+  echo "SonarQube password changed successfully."
 fi
 
-# Start Prometheus
+# Start Prometheus service
 echo "Starting Prometheus..."
 exec prometheus --config.file=/etc/prometheus/prometheus.yml
--- sonarqube/check_sonarqube_health.sh
+++ sonarqube/check_sonarqube_health.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Use wget to fetch the status and check if SonarQube is up
-if wget -qO- http://localhost:9000/api/system/status | grep -q '"status":"UP"'; then
+# Define the URL
+URL="http://localhost:9000/api/system/status"
+
+# Use curl to fetch the status and check if SonarQube is up
+if curl -s "$URL" | grep -q '"status":"UP"'; then
     echo "SonarQube is up and running!"
     exit 0
 else
