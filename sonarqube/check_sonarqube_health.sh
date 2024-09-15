b1d162531428a44739e884861d5d227978e17339
Ioniva <ioniva@icloud.com>
2024-09-15 08:01:47 +0300

    Remove health checker

--- sonarqube/check_sonarqube_health.sh
+++ sonarqube/check_sonarqube_health.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
 # Define the URLs using environment variables
-HEALTH_URL="http://${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/system/health"
 STATUS_URL="http://${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/system/status"
-
-# Function to check if SonarQube is healthy
-check_health() {
-    echo "Checking if SonarQube is fully healthy at $HEALTH_URL..."
-    # Loop until SonarQube returns "GREEN" for health status
-    until wget -qO- "$HEALTH_URL" | grep -q '"status":"GREEN"'; do
-        echo "SonarQube is not fully healthy yet. Retrying in 10 seconds..."
-        sleep 10
-    done
-    echo "SonarQube is fully healthy!"
-}
 
 # Function to check the SonarQube status
 check_status() {
@@ -26,8 +14,7 @@ check_status() {
     echo "SonarQube is up and running!"
 }
 
-# Check health and status of SonarQube
-check_health
+# Check status of SonarQube
 check_status
 
 # Exit with success status
