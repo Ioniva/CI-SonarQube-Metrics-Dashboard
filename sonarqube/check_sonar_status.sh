33e87afe49ad466587a87d11c46f09e189eed53a
Ioniva <ioniva@icloud.com>
2024-09-25 19:36:11 +0300

    Update check sonar status

--- sonarqube/check_sonar_status.sh
+++ sonarqube/check_sonar_status.sh
@@ -4,7 +4,7 @@
 STATUS_URL="http://${SONARQUBE_HOST}:${SONARQUBE_PORT}/api/system/status"
 
 # Fetch system status from the API and check if the status is "UP"
-if wget -qO- "$STATUS_URL" | grep -q '"status": *"UP"'; then
+if wget -qO- $STATUS_URL | grep -q '"status": *"UP"'; then
     exit 0  # Healthy
 else
     exit 1  # Unhealthy
